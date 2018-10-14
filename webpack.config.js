421c2ddeac4954d34e1e32f9f2391fe17703b5dd
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-10-14 14:21:25 -0700

    Adds relay

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
             plugins: [
               '@babel/plugin-proposal-object-rest-spread',
               '@babel/plugin-proposal-class-properties',
+              'babel-plugin-relay',
             ],
             presets: [
               '@babel/preset-env',
